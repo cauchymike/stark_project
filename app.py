9165f45c88cf211d15334734d583ddb4276aba4d
cauchymike <59985204+cauchymike@users.noreply.github.com>
2020-07-26 00:56:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, redirect
 from forms import JobForm
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = 'seunmelody'
+app.config['SECRET_KEY'] = 'seunmelody-is-my-secret-key'
 
 ##################################################################################
 #view section
@@ -12,13 +12,13 @@ app = Flask(__name__)
 def jobformm():
     form = JobForm()
     if form.validate_on_submit():
-        return render_template("success.html")
+        return redirect(url_for('success'))
     return  render_template("index.html", form = form)
 
 #we can also simply return render_template from the above view function 
-#@app.route('/success')
-#def success():
-    #return render_template('success.html')
+@app.route('/success')
+def success():
+    return render_template('success.html')
 
 if __name__ == '__main__':
     app.run()
